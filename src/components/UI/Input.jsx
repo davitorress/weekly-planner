d7e0e6bd18170146a9f011c8c669eb1a44dd642b
davitorress <dev.davitorres@gmail.com>
2023-01-16 17:37:44 -0300

    chore: input icon transition

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -4,6 +4,32 @@ const StyledDiv = styled.div`
 	display: grid;
 	grid-template-columns: 1fr 380px;
 	align-items: center;
+	position: relative;
+
+	${({ iconPath }) =>
+		iconPath
+			? `&::after {
+		content: "";
+		display: block;
+		width: 20px;
+		height: 100%;
+
+		right: 0;
+		z-index: 10;
+		position: absolute;
+
+		transition: 1.5s ease-in-out;
+		transform: translateX(200%);
+
+		background-position: center;
+		background-repeat: no-repeat;
+		background-image: url(${iconPath});
+	}`
+			: ""}
+
+	&:has(input:focus) + &::after {
+		transform: translateX(-150%);
+	}
 `;
 
 const StyledInput = styled.input`
@@ -16,16 +42,18 @@ const StyledInput = styled.input`
 	border-radius: 50px;
 	border: 1px solid #fff;
 
+	${({ label }) => (!label ? `grid-column: 2 span;` : "")}
+
 	&::placeholder {
 		font-size: 1.2rem;
 	}
 `;
 
-const Input = ({ label, type, name, id, placeholder }) => {
+const Input = ({ label, type, name, id, placeholder, iconPath }) => {
 	return (
-		<StyledDiv>
+		<StyledDiv label={label} id={id} iconPath={iconPath}>
 			{label && <label htmlFor={id}>{label}</label>}
-			<StyledInput type={type ? type : "text"} name={name} id={id} placeholder={placeholder} />
+			<StyledInput label={label} type={type ? type : "text"} name={name} id={id} placeholder={placeholder} />
 		</StyledDiv>
 	);
 };
