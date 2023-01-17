383b7b32c22fd5f914619d87d5f5d75db06019e4
davitorress <dev.davitorres@gmail.com>
2023-01-17 14:39:20 -0300

    fix: separate input group and input simple components

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,64 +1,28 @@
 import styled from "styled-components";
 
-const StyledDiv = styled.div`
-	display: grid;
-	grid-template-columns: 1fr 380px;
-	align-items: center;
-	position: relative;
-`;
-
 const StyledInput = styled.input`
 	padding: 18px;
 
-	color: var(--white-secondary);
 	font-size: 1.6rem;
 	font-family: "Mulish", sans-serif;
-
-	border-radius: 50px;
-	border: 1px solid #fff;
-
-	${({ label }) => (!label ? `grid-column: 2 span;` : "")}
-
-	&:focus ~ i,
-	&.input__icon ~ i {
-		transform: translateX(-150%);
-	}
 
 	&::placeholder {
 		font-size: 1.2rem;
 	}
+
+	&.input__action {
+		color: var(--black-secondary);
+		border-radius: 10px;
+		border: 1px solid var(--yellow-action-border);
+	}
+	&:focus.input__action {
+		outline: 2px solid var(--yellow-action-border);
+	}
 `;
 
-const StyledIcon = styled.i`
-	width: 20px;
-	height: 100%;
-
-	right: 0;
-	z-index: 10;
-	position: absolute;
-
-	transition: 1.5s ease-in-out;
-	transform: translateX(200%);
-
-	background-position: center;
-	background-repeat: no-repeat;
-	background-image: url(${({ iconPath }) => iconPath});
-`;
-
-const Input = ({ label, type, name, id, placeholder, className, iconPath }) => {
+const Input = ({ type, name, id, placeholder, className }) => {
 	return (
-		<StyledDiv label={label}>
-			{label && <label htmlFor={id}>{label}</label>}
-			<StyledInput
-				label={label}
-				type={type ? type : "text"}
-				name={name}
-				id={id}
-				placeholder={placeholder}
-				className={className}
-			/>
-			<StyledIcon iconPath={iconPath} />
-		</StyledDiv>
+		<StyledInput type={type ? type : "text"} name={name} id={id} placeholder={placeholder} className={className} />
 	);
 };
 
---
+++ src/components/UI/InputGroup.jsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+import Icon from "./Icon";
+
+const StyledDiv = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 380px;
+	align-items: center;
+	position: relative;
+`;
+
+const StyledInput = styled.input`
+	padding: 18px;
+
+	color: var(--white-secondary);
+	font-size: 1.6rem;
+	font-family: "Mulish", sans-serif;
+
+	border-radius: 50px;
+	border: 1px solid #fff;
+
+	${({ label }) => (!label ? `grid-column: 2 span;` : "")}
+
+	&:focus ~ i,
+	&.input__icon ~ i {
+		transform: translateX(-150%);
+	}
+
+	&::placeholder {
+		font-size: 1.2rem;
+	}
+
+	&.input__account {
+		background-color: #26292c;
+	}
+`;
+
+const InputGroup = ({ label, type, name, id, placeholder, className, iconPath }) => {
+	return (
+		<StyledDiv label={label}>
+			{label && <label htmlFor={id}>{label}</label>}
+			<StyledInput
+				label={label}
+				type={type ? type : "text"}
+				name={name}
+				id={id}
+				placeholder={placeholder}
+				className={className}
+			/>
+			<Icon className="icon__input" iconPath={iconPath} />
+		</StyledDiv>
+	);
+};
+
+export default InputGroup;
