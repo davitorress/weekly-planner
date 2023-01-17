444a08d66794333ac5f4986934fe36e3cd7aa103
davitorress <dev.davitorres@gmail.com>
2023-01-17 14:40:28 -0300

    feat: add Select UI Compnent

---
+++ src/components/UI/Select.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const StyledSelect = styled.select`
+	font-size: 1.6rem;
+	font-family: "Mulish", sans-serif;
+
+	&.select__action {
+		width: 180px;
+		padding: 12px 8px;
+
+		color: var(--black-placeholder);
+
+		border-radius: 10px;
+		border: 1px solid var(--yellow-action-border);
+	}
+	&:focus.select__action {
+		outline: 2px solid var(--yellow-action-border);
+	}
+`;
+
+const Select = ({ children, defaultValue, className }) => {
+	return (
+		<StyledSelect defaultValue={defaultValue} className={className}>
+			{children}
+		</StyledSelect>
+	);
+};
+
+export default Select;
