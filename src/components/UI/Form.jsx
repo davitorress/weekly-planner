0fc9f9314cf6e478edf3a7a0ac6186d361c01d3e
davitorress <dev.davitorres@gmail.com>
2023-01-18 18:17:29 -0300

    style: link section and error message css

--- src/components/UI/Form.jsx
+++ src/components/UI/Form.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const StyledForm = styled.form`
 	&.form__account {
 		display: grid;
-		row-gap: 5rem;
+		row-gap: 3rem;
 
 		color: var(--white-secondary);
 		font-weight: 400;
@@ -29,6 +29,22 @@ const StyledForm = styled.form`
 	&.form__actions .actions__buttons {
 		display: flex;
 		gap: 1rem;
+	}
+
+	.form__link {
+		display: flex;
+		gap: 1rem;
+		padding: 2rem 0;
+		font-size: 1.8rem;
+	}
+
+	p.error-message {
+		margin-top: 2rem;
+
+		color: var(--yellow-error);
+		font-weight: 700;
+		font-size: 1.8rem;
+		text-align: center;
 	}
 `;
 
