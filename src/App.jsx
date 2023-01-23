4f1fc5ebf5fda3a823d5c195017c2a9f437183c5
davitorress <dev.davitorres@gmail.com>
2023-01-23 13:36:20 -0300

    fix: remove user protection for login route

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,18 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 import { AuthContext } from "./store/authContext";
-import { UserContext } from "./store/userContext";
 
 import "./App.css";
 
 function App() {
 	const { isLogged } = useContext(AuthContext);
-	const { username, email, password } = useContext(UserContext);
 
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={isLogged ? <Dashboard /> : <Navigate to="/register" />} />
 				<Route path="/register" element={isLogged ? <Navigate to="/" /> : <Register />} />
-				<Route
-					path="/login"
-					element={
-						isLogged ? <Navigate to="/" /> : username && email && password ? <Login /> : <Navigate to="/register" />
-					}
-				/>
+				<Route path="/login" element={isLogged ? <Navigate to="/" /> : <Login />} />
 			</Routes>
 		</BrowserRouter>
 	);
