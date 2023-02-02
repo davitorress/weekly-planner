8a75056a4aef22d3380ee3ed6ca60aaeb1d67be3
davitorress <dev.davitorres@gmail.com>
2023-02-02 17:28:34 -0300

    refactor: icon generic styled component

--- src/components/styled.jsx
+++ src/components/styled.jsx
@@ -38,3 +38,26 @@ export const StyledAccount = styled.main`
 		background-image: url("/src/assets/back.png");
 	}
 `;
+
+export const Icon = styled.i`
+	width: 20px;
+	height: 100%;
+
+	background-position: center;
+	background-repeat: no-repeat;
+	background-image: url(${({ iconPath }) => iconPath});
+
+	&.icon__input {
+		right: 0;
+		z-index: 10;
+		position: absolute;
+
+		transition: 1.5s ease-in-out;
+		transform: translateX(200%);
+	}
+
+	&.icon__button {
+		height: 20px;
+		display: block;
+	}
+`;
