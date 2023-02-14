c39dd4291aa1b8bf6a969dfedf04250a6ebcf245
davitorress <dev.davitorres@gmail.com>
2023-02-14 15:55:10 -0300

    feat: add ToastContainer to show toasts on screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 import Register from "./pages/Register";
 import Login from "./pages/Login";
@@ -8,18 +9,22 @@ import Dashboard from "./pages/Dashboard";
 import { UserContext } from "./store/userContext";
 
 import "./App.css";
+import "react-toastify/dist/ReactToastify.min.css";
 
 function App() {
 	const { user } = useContext(UserContext);
 
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={user.id ? <Dashboard /> : <Navigate to="/register" />} />
-				<Route path="/register" element={user.id ? <Navigate to="/" /> : <Register />} />
-				<Route path="/login" element={user.id ? <Navigate to="/" /> : <Login />} />
-			</Routes>
-		</BrowserRouter>
+		<>
+			<ToastContainer />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={user.id ? <Dashboard /> : <Navigate to="/register" />} />
+					<Route path="/register" element={user.id ? <Navigate to="/" /> : <Register />} />
+					<Route path="/login" element={user.id ? <Navigate to="/" /> : <Login />} />
+				</Routes>
+			</BrowserRouter>
+		</>
 	);
 }
 
