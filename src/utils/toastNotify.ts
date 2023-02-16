0766b5e84b911edf6b2e2338468e87af9169d6e7
davitorress <dev.davitorres@gmail.com>
2023-02-16 15:31:23 -0300

    style: change auto close to 3s

--- src/utils/toastNotify.ts
+++ src/utils/toastNotify.ts
@@ -5,7 +5,7 @@ type ToastType = "info" | "success" | "warning" | "error";
 const notify = (alertType: ToastType, alertMessage: string) => {
 	toast[`${alertType}`](alertMessage, {
 		position: "top-right",
-		autoClose: 5000,
+		autoClose: 3000,
 		hideProgressBar: false,
 		closeOnClick: true,
 		pauseOnHover: true,
