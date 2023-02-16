9d230df9655446958f221d4440fd122745e4d110
davitorress <dev.davitorres@gmail.com>
2023-02-16 14:42:32 -0300

    fix: remove react stric mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -7,11 +6,9 @@ import { UserProvider } from "./store/userContext";
 import { EventProvider } from "./store/eventContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<UserProvider>
-			<EventProvider>
-				<App />
-			</EventProvider>
-		</UserProvider>
-	</React.StrictMode>
+	<UserProvider>
+		<EventProvider>
+			<App />
+		</EventProvider>
+	</UserProvider>
 );
